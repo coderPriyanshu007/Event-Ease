0a0969b236c89ec8a58a9378cbe845f3fd34aa1f
coderPriyanshu007 <kandaripriyanshu1@gmail.com>
2025-10-02 10:37:02 +0530

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
 
-dotenv.config()
 
-const apiUrl = process.env.VITE_API_URL || 'http://localhost:5000'
 
 
 export default defineConfig({
@@ -14,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: apiUrl,
+        target: 'https://event-ease-05us.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
